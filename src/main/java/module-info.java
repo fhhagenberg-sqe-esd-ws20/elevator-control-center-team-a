6937a29254a7e541b4c380f126524bcc88f0641e
Paul <paul70079@gmail.com>
2021-01-05 19:28:36 +0100

    add required modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module at.fhhagenberg.sqe {
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires javafx.base;
-	
+    requires org.jetbrains.annotations;
+    requires java.rmi;
+
     exports at.fhhagenberg.esd.sqe.ws20.gui;
     opens at.fhhagenberg.esd.sqe.ws20.gui to javafx.fxml;
 }
