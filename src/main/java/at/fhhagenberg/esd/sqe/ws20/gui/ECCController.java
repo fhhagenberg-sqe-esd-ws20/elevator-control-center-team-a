e09680cdf2736eba638fe5eb59884b20a0825e29
Mathias Dittrich <MathiasDittrich@gmx.at>
2021-01-14 13:39:14 +0100

    +Fix: implemented uncommited direction the right way

--- src/main/java/at/fhhagenberg/esd/sqe/ws20/gui/ECCController.java
+++ src/main/java/at/fhhagenberg/esd/sqe/ws20/gui/ECCController.java
@@ -44,7 +44,7 @@ public class ECCController implements Initializable {
     private final Timer timer = new Timer();
     private final StringProperty errorText = new SimpleStringProperty("");
     private final BooleanProperty isConnected = new SimpleBooleanProperty(false);
-    private final BooleanProperty isOnTargetFloor = new SimpleBooleanProperty(false);
+    private final BooleanProperty isUncommitedDirection = new SimpleBooleanProperty(false);
     @SuppressWarnings("unused")
     @FXML
     private ComboBox<String> cbElevator;
@@ -288,7 +288,7 @@ public class ECCController implements Initializable {
             isDoorOpen.setValue(elevatorState.getCurrentDoorState() == DoorState.OPEN);
             isDirectionUp.setValue(elevatorState.getCurrentDirection() == Direction.UP);
             targetFloor.setValue(elevatorState.getTargetFloor());
-            isOnTargetFloor.setValue(elevatorState.getCurrentFloor() == elevatorState.getTargetFloor());
+            isUncommitedDirection.setValue(elevatorState.getCurrentDirection() == Direction.UNCOMMITTED);
 
             var servicedFloors = elevatorState.getServicedFloors();
 
@@ -329,9 +329,9 @@ public class ECCController implements Initializable {
         ivDoorStateClosed.visibleProperty().bind(isDoorOpen.not().and(anyElevatorSelected).and(isConnected));
         ivDoorStateOpen.visibleProperty().bind(isDoorOpen.and(anyElevatorSelected).and(isConnected));
 
-        ivGElvDirUp.visibleProperty().bind(isDirectionUp.and(anyElevatorSelected).and(isOnTargetFloor.not()));
+        ivGElvDirUp.visibleProperty().bind(isDirectionUp.and(anyElevatorSelected).and(isUncommitedDirection.not()));
         ivGElvDirUp.disableProperty().bind(isConnected.not());
-        ivGElvDirDown.visibleProperty().bind(isDirectionUp.not().and(anyElevatorSelected).and(isConnected).and(isOnTargetFloor.not()));
+        ivGElvDirDown.visibleProperty().bind(isDirectionUp.not().and(anyElevatorSelected).and(isConnected).and(isUncommitedDirection.not()));
         ivGElvDirDown.disableProperty().bind(isConnected.not());
 
         cbTargetFloor.itemsProperty().bind(floorNames);
