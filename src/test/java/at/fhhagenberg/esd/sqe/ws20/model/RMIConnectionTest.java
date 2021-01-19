d88955c618b17e8bb208a7a5a809b8fc4f6ac404
Sebastian Kaupper <kauppersebastian@gmail.com>
2021-01-19 11:44:32 +0100

    be more specific with expected exceptions during RMIConnectionTests

--- src/test/java/at/fhhagenberg/esd/sqe/ws20/model/RMIConnectionTest.java
+++ src/test/java/at/fhhagenberg/esd/sqe/ws20/model/RMIConnectionTest.java
@@ -6,6 +6,7 @@ import at.fhhagenberg.esd.sqe.ws20.utils.ManagedIElevator;
 import org.junit.jupiter.api.Test;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -21,7 +22,7 @@ class RMIConnectionTest {
     @Test
     void testEndpointNotAvailable() {
         final String unavailableUrl = "rmi://test/hopefullyUnavailableEndpoint";
-        assertThrows(Exception.class, () -> Naming.list(unavailableUrl), "An endpoint for the given URL has been found, although non was expected.");
+        assertThrows(RemoteException.class, () -> Naming.list(unavailableUrl), "An endpoint for the given URL has been found, although non was expected.");
 
         var impl = new ElevatorImpl(new ManagedIElevator(unavailableUrl));
         assertThrows(ConnectionError.class, impl::queryGeneralInformation);
